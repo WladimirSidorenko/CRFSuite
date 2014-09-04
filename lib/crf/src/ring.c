55cc8a1159d1e7ef036d0310199b2ad944e4f942
Wladimir Sidorenko <wlsidorenko@gmail.com>
2014-09-04 23:36:02 +0200

    ring files added

---
+++ lib/crf/src/ring.c
@@ -0,0 +1,89 @@
+/*
+ *      Implementation of dictionary.
+ *
+ * Copyright (c) 2007-2010, Uladzimir Sidarenka
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the names of the authors nor the names of its contributors
+ *       may be used to endorse or promote products derived from this
+ *       software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* $Id$ */
+
+#include "ring.h"
+
+// Push an element in the ring.
+static int crfsuite_ring_push(crfsuite_ring_t *a_ring, int a_el)
+{
+  if (a_ring->max_items == 0)
+    return -1;
+
+  /* if we overflow the buffer, move the head of ringed queue */
+  if (a_ring->n_items >= a_ring->max_items)
+    ++a_ring->head;
+  else
+    ++a_ring->n_items;
+
+  /* if tail or head go beyond array boundaries, reset them to the beginning
+     of the array */
+  if (++a_ring->tail >= a_ring->end)
+    a_ring->tail = a_ring->internal;
+
+  if (++a_ring->head >= a_ring->end)
+    a_ring->head = a_ring->internal;
+
+  *a_ring->tail = a_el;
+  return 0;
+}
+
+// Clear ring and reset pointers.
+static void crfsuite_ring_free(crfsuite_ring_t *a_ring)
+{
+  free(a_ring->internal);
+  a_ring->internal = a_ring->end = NULL;
+  a_ring->head = a_ring->tail = NULL;
+  a_ring->max_items = a_ring->num_items = 0;
+}
+
+int crfsuite_ring_create_instance(crfsuite_ring_t **a_ring, int a_size)
+{
+  crfsuite_ring_t* iring = (crfsuite_ring_t*) calloc(1, sizeof(crfsuite_ring_t));
+
+  if (iring != NULL) {
+    iring->n_items = 0;
+    iring->internal = (int *) calloc(a_size, sizeof(int));
+    if (iring->internal == NULL)
+      return 1;
+
+    iring->max_items = a_size;
+    iring->end = iring->internal + iring->max_items;
+    iring->head = iring->tail = iring->internal;
+    iring->push = crfsuite_ring_push;
+    iring->free = crfsuite_ring_free;
+
+    *a_ring = iring;
+    return 0;
+  } else {
+    return -1;
+  }
+}
---
+++ lib/crf/src/ring.h
@@ -0,0 +1,99 @@
+/*
+ *      Ring object.
+ *
+ * Copyright (c) 2007-2010, Uladzimir Sidarenka
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the names of the authors nor the names of its contributors
+ *       may be used to endorse or promote products derived from this
+ *       software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* $Id$ */
+
+#ifndef    __RING_H__
+# define    __RING_H__
+
+/**
+ * \addtogroup crfsuite_object Object interfaces and utilities.
+ * @{
+ */
+
+/**
+ * CRFSuite ring interface.
+ */
+struct tag_crfsuite_ring;
+typedef struct tag_crfsuite_ring crfsuite_ring_t;
+
+/**
+ * Create an instance of \ref crfsuite_ring_t.
+ *
+ * @param a_ring - address of pointer which should point to newly created ring
+ * @param a_size - maximum number of elements to store in ring
+ *
+ * @return \c 0 on success and non-\c 0 otherwise
+ */
+int crfsuite_ring_create_instance(crfsuite_ring_t **a_ring, int a_size);
+
+struct tag_crfsuite_ring {
+  /**
+   * Pointer to instance data (internal use only).
+   */
+  int *internal;
+
+  /**
+   * Pointer to the initial element of the ring.
+   */
+  int *head;
+
+  /**
+   * Pointer to the last element of the ring.
+   */
+  int *tail;
+
+  /**
+   * Add element to the ring.
+   *
+   * @param a_ring - queue instance to which element should be added
+   * @param a_el - element to push in the ring
+   */
+  int (*push)(crfsuite_ring_t *a_ring, int a_el);
+
+  /**
+   * Clear elements in the ring.
+   *
+   * @param a_ring - pointer to queue instance to be freed
+   */
+  void (*free)(crfsuite_ring_t *a_ring);
+
+  /**
+   * Maximum number of items to store in the ring.
+   */
+  int max_items;
+
+  /**
+   * Actual number of stored items.
+   */
+  int n_items;
+};
+/**@}*/
+#endif/*__RING_H__*/
